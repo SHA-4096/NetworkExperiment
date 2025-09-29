3a45e8d778d2aebeeb385e47aad2b57976ddaf5b
Zhanchao Xu <xuzhanchaomail@163.com>
2025-09-29 21:04:28 +0800

    update: remove query name
    Signed-off-by: Zhanchao Xu <xuzhanchaomail@163.com>

--- dns.py
+++ dns.py
@@ -17,7 +17,6 @@ def init_db():
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
             src_ip TEXT,
             dst_ip TEXT,
-            query_name TEXT,
             query_type INTEGER
         )
     ''')
@@ -25,14 +24,14 @@ def init_db():
     conn.close()
 
 
-def store_query(src_ip_in, dst_ip_in, query_name_in, query_type_in):
+def store_query(src_ip_in, dst_ip_in, query_type_in):
     print("[*] Storing query in database...")
     conn = sqlite3.connect('dns_queries.db')
     c = conn.cursor()
     c.execute(f'''
-        INSERT INTO dns_queries (src_ip, dst_ip, query_name, query_type)
-        VALUES (?, ?, ?, ?)
-    ''', (src_ip_in, dst_ip_in, query_name_in, query_type_in))
+        INSERT INTO dns_queries (src_ip, dst_ip, query_type)
+        VALUES (?, ?, ?)
+    ''', (src_ip_in, dst_ip_in,  query_type_in))
     conn.commit()
     print("[*] Query stored successfully.")
     conn.close()
@@ -62,12 +61,6 @@ def packet_callback(packet):
     if packet.haslayer(DNS) and packet.haslayer(DNSQR):
     # 确保这是一个DNS查询
         if packet[DNS].qr == 0:  # qr=0表示这是一个查询
-            # 获取查询的域名
-            if charset is None:
-                detected = chardet.detect(packet[DNSQR].qname)
-                charset = detected['encoding'] if detected['encoding'] else 'utf-8'
-                print(f"[*] Detected charset: {charset}")
-            query_name = str(packet[DNSQR].qname.decode(charset))
             # 获取查询类型
             query_type = str(packet[DNSQR].qtype)
             # 获取源IP和目标IP
@@ -77,11 +70,10 @@ def packet_callback(packet):
             print(f"[*] DNS Query:")
             print(f"    Source IP: {src_ip}")
             print(f"    Destination IP: {dst_ip}")
-            print(f"    Query Name: {query_name}")
             print(f"    Query Type: {query_type}")
             print("-" * 50)
             # 存储查询到数据库并统计
-            store_query(src_ip, dst_ip, query_name, query_type)
+            store_query(src_ip, dst_ip, query_type)
             count_query_by_dns_server()
 
 def main():
